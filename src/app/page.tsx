f5279b546a430d93baebb0b6637e77d7986378cb
vanis <pavlik.vanis@email.cz>
2023-12-11 08:34:22 +0100

    Update Bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,5 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 export default function Home() {
-  return (
-    <main className="">
-      Hello chatio
-    </main>
-  )
+  return <></>;
 }
--- src/components/bar/bar.tsx
+++ src/components/bar/bar.tsx
@@ -1,10 +1,11 @@
+import { HomeIcon } from 'lucide-react';
 import React from 'react';
 
 const Bar = () => {
     return (
         // Left side bar
-        <div className="bar">
-            bar
+        <div className="bar p-3">
+            <HomeIcon className='w-7 h-7'/>
         </div>
     );
 };
