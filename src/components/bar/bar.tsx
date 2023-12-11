3c38f02a6f4b93527fdcbb92a6bc41d27972cc3f
vanis <pavlik.vanis@email.cz>
2023-12-11 08:20:39 +0100

    Create left Bar

---
+++ src/components/bar/bar.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Bar = () => {
+    return (
+        // Left side bar
+        <div className="bar">
+            bar
+        </div>
+    );
+};
+
+export default Bar;
