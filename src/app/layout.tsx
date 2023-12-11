b02b912886eab837f11a520c94f13cd539b03608
vanis <pavlik.vanis@email.cz>
2023-12-11 08:21:06 +0100

    Update RootLayuout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,26 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { Bar } from "@/components/";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'ChatIo',
-  description: 'Realtime chat app',
-}
+  title: "ChatIo",
+  description: "Realtime chat app",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Bar />
+        <main>{children}</main>
+      </body>
     </html>
-  )
+  );
 }
