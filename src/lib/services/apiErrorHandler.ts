ac1d99b2ed74df4e7f2689300d2b4d5acd7c00c6
vanis <pavlik.vanis@email.cz>
2023-12-12 13:59:15 +0100

    Create error handler

---
+++ src/lib/services/apiErrorHandler.ts
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+import { NextResponse } from "next/server";
+
+export const errorHandler = (error: any) => {
+  console.error(error);
+  if (error instanceof mongoose.Error.ValidationError) {
+    return NextResponse.json({ message: error.message }, { status: 400 });
+  }
+  return NextResponse.json({ message: error }, { status: 400 });
+};
