4813800b42934eed60347e17ea7a7b817b11f50e
vanis <pavlik.vanis@email.cz>
2023-12-12 11:11:03 +0100

    Login validation

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -1,12 +1,24 @@
+import connectDB from "@/lib/mongo";
+import UserModel from "@/models/user";
 import { NextRequest, NextResponse } from "next/server";
 
 export const POST = async (req: NextRequest) => {
   try {
     const { email, password } = await req.json();
 
-    // ... Check if user exist in database
+    await connectDB();
 
-    // ... Check if password match
+    // Check if user exist in database ..
+    const user = await UserModel.findOne({ email });
+    if (!user) {
+      return NextResponse.json({ message: "User not found" }, { status: 404 });
+    }
+
+    // Check if password match ..
+    const pwdMatch = await user.comparePassword(password);
+    if(!pwdMatch) {
+      return NextResponse.json({ message: "Password not match" }, { status: 401 });
+    }
 
     // ... Create JWT token
 
