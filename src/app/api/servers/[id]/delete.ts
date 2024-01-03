5d462f7d7854afbf5f673420e0e764e76ea79c43
vanis <pavlik.vanis@email.cz>
2024-01-03 17:17:03 +0100

    Update deleting server by id

--- src/app/api/servers/[id]/delete.ts
+++ src/app/api/servers/[id]/delete.ts
@@ -1,10 +1,26 @@
-import { NextRequest } from "next/server";
+import { errorHandler } from "@/lib/services/apiErrorHandler";
+import ServerModel from "@/models/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export const deleteOne = async (req: NextRequest) => {
-    try {
-        
-    } catch (error) {
-        
-    }
+type Props = {
+  params: {
+    id: string;
+  };
 };
 
+export const deleteOne = async (
+  req: NextRequest,
+  { params: { id } }: Props
+) => {
+  try {
+
+    const deleted = await ServerModel.findByIdAndDelete(id);
+    if (!deleted) {
+      return NextResponse.json({ message: "Server not found" }, { status: 404 });
+    }
+
+    return NextResponse.json(deleted);
+  } catch (error) {
+    return errorHandler(error);
+  }
+};
