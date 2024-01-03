777482c92736b6d06e1eece9b9102c7f206c0c26
vanis <pavlik.vanis@email.cz>
2024-01-03 14:11:21 +0100

    Refactor left side bar to display server icons

--- src/components/bar/bar.tsx
+++ src/components/bar/bar.tsx
@@ -1,13 +1,20 @@
-import { HomeIcon } from 'lucide-react';
-import React from 'react';
+import serversData from "@/test-data/servers";
+import { HomeIcon } from "lucide-react";
+import React from "react";
+import ServerIconButton from "../buttons/server";
 
 const Bar = () => {
-    return (
-        // Left side bar
-        <div className="bar p-3">
-            <HomeIcon className='w-7 h-7'/>
-        </div>
-    );
+  return (
+    // Left side bar with servers
+    <div className="bar p-3 flex flex-col gap-2">
+      <ServerIconButton>
+        <HomeIcon className="w-[1.65rem] h-[1.65rem]" strokeWidth={1.6} />
+      </ServerIconButton>
+      {serversData.map((server) => (
+        <ServerIconButton key={server.id} {...server} />
+      ))}
+    </div>
+  );
 };
 
 export default Bar;
