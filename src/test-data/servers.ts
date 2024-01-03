667e73827ef412629b221de54f744ba2df01d6ca
vanis <pavlik.vanis@email.cz>
2024-01-03 14:01:42 +0100

    create testing data

---
+++ src/test-data/servers.ts
@@ -0,0 +1,56 @@
+const serversData = [
+    {
+      "id": "dwdwf21r1g42fqcw",
+      "image": "www.some-url-to-picture.com",
+      "name": "Server 1",
+      "ip": "192.168.0.1",
+      "port": 8080
+    },
+    {
+      "id": "dwdwf21r1g423qcw",
+      "image": "www.some-url-to-picture.com",
+      "name": "Server 2",
+      "ip": "192.168.0.2",
+      "port": 8080
+    },
+    {
+      "id": "dwdwf21r1g424qcw",
+      "image": "www.some-url-to-picture.com",
+      "name": "Server 3",
+      "ip": "192.168.0.3",
+      "port": 8080
+    },
+    {
+      "id": "dwdwf21r1g424qcw",
+      "image": "www.some-url-to-picture.com",
+      "name": "Server 3",
+      "ip": "192.168.0.3",
+      "port": 8080
+    },
+    {
+      "id": "dwdwf21r1g424qcw",
+      "image": "www.some-url-to-picture.com",
+      "name": "Server 3",
+      "ip": "192.168.0.3",
+      "port": 8080
+    },
+    {
+      "id": "dwdwf21r1g424qcw",
+      "image": "www.some-url-to-picture.com",
+      "name": "Server 3",
+      "ip": "192.168.0.3",
+      "port": 8080
+    },
+    {
+      "id": "dwdwf21r1g424qcw",
+      "image": "www.some-url-to-picture.com",
+      "name": "Server 3",
+      "ip": "192.168.0.3",
+      "port": 8080
+    },
+  ];
+
+  export default serversData;
+  
+  // Nyní máte proměnnou serversData obsahující uvedená data o serverech.
+  
