420f43b119d1a3d350e6d553c71296ca94097b55
vanis <pavlik.vanis@email.cz>
2024-01-03 16:04:27 +0100

    Updates

--- src/app/api/servers/[id]/getOne.ts
+++ src/app/api/servers/[id]/getOne.ts
@@ -1,4 +1,6 @@
+import connectDB from "@/lib/mongo";
 import { errorHandler } from "@/lib/services/apiErrorHandler";
+import ServerModel from "@/models/server";
 import UserModel from "@/models/user";
 import { NextRequest, NextResponse } from "next/server";
 
@@ -11,14 +13,16 @@ type Props = {
 export const getOne = async (req: NextRequest, { params: { id } }: Props) => {
   try {
 
+    await connectDB();
+
     //
     // Check JWT
     //
 
     // Get All Users ..
-    const user = await UserModel.findById(id);
+    const user = await ServerModel.findById(id);
     if (!user) {
-      return NextResponse.json({ message: "User not found" }, { status: 404 });
+      return NextResponse.json({ message: "Server not found" }, { status: 404 });
     }
     // ..
 
