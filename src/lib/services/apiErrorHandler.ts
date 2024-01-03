a315c46019ea5dc2a4730ed11a00cb9ef579eb6c
vanis <pavlik.vanis@email.cz>
2024-01-03 16:05:43 +0100

    Updates

--- src/lib/services/apiErrorHandler.ts
+++ src/lib/services/apiErrorHandler.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 
 export const errorHandler = (error: any) => {
   console.error(error);
-  if (error instanceof mongoose.Error) {
+  if (error instanceof mongoose.Error.ValidationError) {
     return NextResponse.json({ message: error.message }, { status: 400 });
   }
   return NextResponse.json({ message: error }, { status: 400 });
