af6aca5c7b957ad06baf3eb632cc4c440b2a97a3
vanis <pavlik.vanis@email.cz>
2024-01-04 21:26:06 +0100

    Add redirect when user is logged

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,10 +1,13 @@
+import authOptions from "@/lib/authoptions";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 
 export default async function LoginLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return (
-      <main>{children}</main>
-  );
+  const session = await getServerSession(authOptions);
+  if(session) return redirect("/");
+  return <main>{children}</main>;
 }
