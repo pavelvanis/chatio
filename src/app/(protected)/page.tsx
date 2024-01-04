746f7734d21001949e6e539db9fa3e9c0263148c
vanis <pavlik.vanis@email.cz>
2024-01-04 12:42:00 +0100

    Test SocketIndicator

--- src/app/(protected)/page.tsx
+++ src/app/(protected)/page.tsx
@@ -1,9 +1,10 @@
+import { SocketIndicator } from "@/components/ui/socket-indicator";
+
 export default function Home() {
-
-
   return (
     <div className="flex grow justify-center ">
       <h1>Home</h1>
+      <SocketIndicator />
     </div>
   );
 }
