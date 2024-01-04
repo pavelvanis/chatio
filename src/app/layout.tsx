c4b83a00742241d8e965334d86a04d5fcd8c3a32
vanis <pavlik.vanis@email.cz>
2024-01-04 21:25:36 +0100

    Update providers

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <SocketProvider>
-        <body className={inter.className}>{children}</body>
+          <body className={inter.className}>{children}</body>
       </SocketProvider>
     </html>
   );
