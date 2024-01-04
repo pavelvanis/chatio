238659873535de22ea3a7cfc7ae3a0c25031c79c
vanis <pavlik.vanis@email.cz>
2024-01-04 21:26:23 +0100

    Add Authprovider

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,20 +1,23 @@
 import MembersBar from "@/components/members/members-bar";
 import ProfileBar from "@/components/profile/profile-bar";
+import AuthProvider from "@/components/providers/auth-provider";
 import ServersBar from "@/components/servers/servers-bar";
 import React from "react";
 
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="h-full flex">
-      <div className="grow">
-        <ProfileBar />
-        <div className="h-full flex max-h-[calc(100vh-3rem)]">
-          <ServersBar />
-          {children}
+    <AuthProvider>
+      <div className="h-full flex">
+        <div className="grow">
+          <ProfileBar />
+          <div className="h-full flex max-h-[calc(100vh-3rem)]">
+            <ServersBar />
+            {children}
+          </div>
         </div>
+        <MembersBar />
       </div>
-      <MembersBar />
-    </div>
+    </AuthProvider>
   );
 };
 
