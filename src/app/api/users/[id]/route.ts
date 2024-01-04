b93aacb6f13e4713f65b51162a24898bcd610ab4
vanis <pavlik.vanis@email.cz>
2024-01-04 11:28:35 +0100

    Add DELETE option

--- src/app/api/users/[id]/route.ts
+++ src/app/api/users/[id]/route.ts
@@ -1,4 +1,5 @@
 import { getOne } from "./getOne";
 import { update } from "./update";
+import { deleteOne } from "./delete";
 
-export { getOne as GET, update as PATCH };
+export { getOne as GET, update as PATCH, deleteOne as DELETE };
