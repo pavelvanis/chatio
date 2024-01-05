f70145a877c86c1c23535b5885cbafcf4ffdab6d
vanis <pavlik.vanis@email.cz>
2024-01-05 12:45:34 +0100

    Oveerride Session interface in next-auth

---
+++ nextauth.d.ts
@@ -0,0 +1,9 @@
+import { IUser } from "@/models/user";
+import { DefaultSession } from "next-auth";
+
+declare module "next-auth" {
+  interface Session extends IUser {
+    user: IUser;
+    token: string;
+  }
+}
