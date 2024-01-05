2aa82a1292f8adf43085bf9fc3d69f61a2ae53a9
vanis <pavlik.vanis@email.cz>
2024-01-05 12:48:45 +0100

    Add SessionProvider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { SocketProvider } from "@/components/providers/socket-provider";
+import SessionProvider from "@/components/providers/session-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +19,9 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <SocketProvider>
+        <SessionProvider>
           <body className={inter.className}>{children}</body>
+        </SessionProvider>
       </SocketProvider>
     </html>
   );
