186df3a528a5ebde19ae2f65d01428156f7f3c7d
vanis <pavlik.vanis@email.cz>
2024-01-05 22:15:31 +0100

    Get and show all servers

--- src/components/servers/servers-bar.tsx
+++ src/components/servers/servers-bar.tsx
@@ -6,21 +6,44 @@ import { getServerSession } from "next-auth";
 import React from "react";
 import { twMerge } from "tailwind-merge";
 
+import ServerModel, { IServer } from "@/models/server";
+import ServerItem from "./server-item";
+
 const getServers = async () => {
-  const session = await getServerSession(authOptions)
-  const memberships = await MemberShipModel.find({user: session?.user?.id}).populate('server');
-  console.log(memberships);
-  return serversData;
+  const session = await getServerSession(authOptions);
+  const memberships = await MemberShipModel.find({
+    user: session?.user?.id,
+  });
+  const servers = await Promise.all(
+    memberships.map(
+      async (membership) => await ServerModel.findById(membership.server)
+    )
+  );
+  return servers as IServer[];
 };
 
 type Props = React.HTMLProps<HTMLDivElement> & {};
 
 const ServersBar: React.FC<Props> = async ({ className }) => {
   const servers = await getServers();
-  // console.log("servers:", servers);
+  console.log("servers:", servers);
+  // console.log(servers[0].name);
   return (
-    <nav className={twMerge(" h-full w-40 bg-slate-300", className)}>
-      ServersBar
+    <nav className={twMerge(" h-full w-40 bg-slate-300 flex flex-col", className)}>
+      {servers.map((server, index) => (
+        <ServerItem
+          key={index}
+          {...{
+            id: server.id,
+            name: server.name,
+            description: server.description,
+            private: server.private,
+            inviteCode: server.inviteCode,
+            owner: server.owner,
+          }}
+        />
+        // <div key={index}>{server.name}</div>
+      ))}
     </nav>
   );
 };
