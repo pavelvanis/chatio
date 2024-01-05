9b9e811120462e7e3e4876e6eae93c4bd2810ab0
vanis <pavlik.vanis@email.cz>
2024-01-05 13:27:28 +0100

    Updates

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,23 +1,29 @@
 import MembersBar from "@/components/members/members-bar";
 import ProfileBar from "@/components/profile/profile-bar";
-import AuthProvider from "@/components/providers/auth-provider";
 import ServersBar from "@/components/servers/servers-bar";
+import authOptions from "@/lib/authoptions";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 import React from "react";
 
-const MainLayout = ({ children }: { children: React.ReactNode }) => {
+const MainLayout = async ({ children }: { children: React.ReactNode }) => {
+  const session = await getServerSession(authOptions);
+
+  console.log("session", session);
+
+  // Redirect when user is not logged in
+  if (!session) return redirect("/login");
   return (
-    <AuthProvider>
-      <div className="h-full flex">
-        <div className="grow">
-          <ProfileBar />
-          <div className="h-full flex max-h-[calc(100vh-3rem)]">
-            <ServersBar />
-            {children}
-          </div>
+    <div className="h-full flex">
+      <div className="grow">
+        <ProfileBar />
+        <div className="h-full flex max-h-[calc(100vh-3rem)]">
+          <ServersBar />
+          {children}
         </div>
-        <MembersBar />
       </div>
-    </AuthProvider>
+      <MembersBar />
+    </div>
   );
 };
 
