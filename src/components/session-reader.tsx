e05df59deb5933d88a70784df6fd071492bde166
vanis <pavlik.vanis@email.cz>
2024-01-05 12:46:38 +0100

    Create component to test Session

---
+++ src/components/session-reader.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { useSession } from "next-auth/react";
+import React from "react";
+
+const SessionReader = () => {
+  const session = useSession();
+  console.log(session);
+  return <>{session?.data?.user?.email}</>;
+};
+
+export default SessionReader;
