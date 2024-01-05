6abe75ad528a32654bd93330847c6eb34cc9bd1e
vanis <pavlik.vanis@email.cz>
2024-01-05 12:47:26 +0100

    Add testing SessionReader

--- src/components/profile/profile-bar.tsx
+++ src/components/profile/profile-bar.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
+import SessionReader from "../session-reader";
 
 const ProfileBar = () => {
   return (
-    <header className='w-full h-12 bg-slate-200'>ProfileBar</header>
-  )
-}
+    <header className="w-full h-12 bg-slate-200">
+      ProfileBar
+      <SessionReader />
+    </header>
+  );
+};
 
-export default ProfileBar
+export default ProfileBar;
