350bee394165fc6f45e91e4440cca65ac76e5c4b
vanis <pavlik.vanis@email.cz>
2024-01-06 17:40:57 +0100

    Create handler for craeting messages in socket

---
+++ src/pages/api/socket/messages.ts
@@ -0,0 +1,63 @@
+import authOptions from "@/lib/authoptions";
+import MemberShipModel from "@/models/member-ship";
+import ServerModel from "@/models/server";
+import { NextApiResponseServerIO } from "@/types";
+import { NextApiRequest } from "next";
+import { getServerSession } from "next-auth";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponseServerIO
+) {
+  // Hadnle only POST requests
+  if (req.method !== "POST") {
+    return res.status(405).json({ error: "Method not allowed" });
+  }
+
+  //   Get session
+  const session = await getServerSession(req, res, authOptions);
+
+  try {
+    const user = session?.user;
+    const { content } = req.body;
+    const { serverId } = req.query;
+
+    // Check if user is authenticated
+    if (!user) {
+      return res.status(401).json({ error: "Unauthorized" });
+    }
+
+    // Check if content is provided
+    if (!content) {
+      return res.status(400).json({ error: "Content is required" });
+    }
+
+    // Check if serverId is provided
+    if (!serverId) {
+      return res.status(400).json({ error: "Server ID is required" });
+    }
+
+    const server = await ServerModel.findById(serverId);
+
+    if (!server) {
+      return res.status(404).json({ error: "Server not found" });
+    }
+
+    const memberShip = await MemberShipModel.findOne({
+      user: user.id,
+      server: serverId,
+    });
+
+    if (!memberShip) {
+      return res
+        .status(403)
+        .json({ error: "User is not member of this server" });
+    }
+
+    
+
+  } catch (error) {
+    console.error("[MESSAGES_POST]", error);
+    return res.status(500).json({ error: "Internal server error" });
+  }
+}
