cbad77b1be6d509a62c303214738640829e0f4b9
vanis <pavlik.vanis@email.cz>
2024-01-06 20:47:18 +0100

    Add QueryProvider to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { SocketProvider } from "@/components/providers/socket-provider";
 import SessionProvider from "@/components/providers/session-provider";
 import { ServerProvider } from "@/components/providers/server-provider";
+import QueryProvider from "@/components/providers/query-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -27,7 +28,9 @@ export default function RootLayout({
       <SocketProvider>
         <SessionProvider>
           <ServerProvider>
-            <body className={inter.className}>{children}</body>
+            <QueryProvider>
+              <body className={inter.className}>{children}</body>
+            </QueryProvider>
           </ServerProvider>
         </SessionProvider>
       </SocketProvider>
