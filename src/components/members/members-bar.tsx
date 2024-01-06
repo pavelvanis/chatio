c1979f45f00fba23cc14ae6eae9155ca2b10556e
vanis <pavlik.vanis@email.cz>
2024-01-06 14:35:10 +0100

    Mapping server members

--- src/components/members/members-bar.tsx
+++ src/components/members/members-bar.tsx
@@ -1,17 +1,23 @@
+"use client";
 import React from "react";
 import { twMerge } from "tailwind-merge";
-
-const getMembers = async () => {
-  const members = [];
-  return [];
-}
+import { useServer } from "../providers/server-provider";
+import MemberItem from "./member-item";
 
 type Props = React.HTMLProps<HTMLDivElement> & {};
 
 const MembersBar: React.FC<Props> = ({ className, ...props }) => {
+  const { server, members } = useServer();
+
   return (
-    <div {...props} className={twMerge("h-full w-48 bg-slate-50", className)}>
-      MembersBar
+    <div
+      {...props}
+      className={twMerge("h-full w-48 bg-slate-50 p-3", className)}
+    >
+      <h3 className="m-0 mb-3">Members:</h3>
+      {members?.map((member, index) => (
+        <MemberItem key={index} {...member} />
+      ))}
     </div>
   );
 };
