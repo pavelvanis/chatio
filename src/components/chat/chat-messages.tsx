d75d003c4d03dbe78e0031c948e22006d8c00c42
vanis <pavlik.vanis@email.cz>
2024-01-07 16:45:42 +0100

    Implement useChatScrolling

--- src/components/chat/chat-messages.tsx
+++ src/components/chat/chat-messages.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useRef } from "react";
 
 import { format } from "date-fns";
 import { useChatQuery } from "@/hooks/use-chat-query";
@@ -14,6 +14,7 @@ import { IUser } from "@/models/user";
 import ChatWelcome from "./chat-welcome";
 import ChatItem from "./chat-item";
 import { useChatSocket } from "@/hooks/use-socket";
+import { useChatScroll } from "@/hooks/use-chat-scroll";
 
 const DATE_FORMAT = "d MMM yyyy, HH:mm";
 
@@ -31,6 +32,9 @@ const ChatMasseges: React.FC<ChatMassegesProps> = ({ apiUrl, socketUrl }) => {
   const addKey = `chat:${server?.id}:messages`;
   const updateKey = `chat:${server?.id}:message:update`;
 
+  const chatRef = useRef<HTMLDivElement>(null);
+  const bottomRef = useRef<HTMLDivElement>(null);
+
   const { data, fetchNextPage, hasNextPage, isFetchingNextPage, status } =
     useChatQuery({
       queryKey,
@@ -39,7 +43,14 @@ const ChatMasseges: React.FC<ChatMassegesProps> = ({ apiUrl, socketUrl }) => {
       paramValue: server?.id,
     });
 
-    useChatSocket({queryKey, updateKey, addKey })
+  useChatSocket({ queryKey, updateKey, addKey });
+  useChatScroll({
+    chatRef,
+    bottomRef,
+    loadMore: fetchNextPage,
+    shouldLoadMore: !isFetchingNextPage && !!hasNextPage,
+    count: data?.pages?.[0].items.length ?? 0,
+  });
 
   if (status === "loading") {
     return (
@@ -52,7 +63,7 @@ const ChatMasseges: React.FC<ChatMassegesProps> = ({ apiUrl, socketUrl }) => {
 
   if (status === "error") {
     return (
-      <div className="grow flex flex-col justify-center items-center">
+      <div className="flex-1 flex flex-col justify-center items-center">
         <ServerCrash className="w-7 h-7 text-zinc-500 my-4" />
         <p className=" text-zinc-500 m-0 text-sm">Something went wrong!</p>
       </div>
@@ -60,9 +71,24 @@ const ChatMasseges: React.FC<ChatMassegesProps> = ({ apiUrl, socketUrl }) => {
   }
 
   return (
-    <div className="grow flex flex-col overflow-y-auto pb-4 p-2">
-      <div className="grow" />
-      <ChatWelcome name={server?.name} />
+    <div ref={chatRef} className="flex-1 flex flex-col overflow-y-auto">
+      {!hasNextPage && <div className="flex-1" />}
+      {!hasNextPage && <ChatWelcome name={server?.name} />}
+      {hasNextPage && (
+        <div className="flex justify-center my-4">
+          {isFetchingNextPage ? (
+            <Loader2 className="w-7 h-7 text-zinc-500 animate-spin" />
+          ) : (
+            <button
+              className="text-zinc-500 hover:text-zinc-600 text-sm border-none 
+              bg-transparent cursor-pointer transition-all"
+              onClick={() => fetchNextPage()}
+            >
+              Load previous messages
+            </button>
+          )}
+        </div>
+      )}
       <div className="flex flex-col-reverse mt-auto">
         {data?.pages.map((page, i) => (
           <React.Fragment key={i}>
@@ -78,6 +104,7 @@ const ChatMasseges: React.FC<ChatMassegesProps> = ({ apiUrl, socketUrl }) => {
           </React.Fragment>
         ))}
       </div>
+      <div ref={bottomRef} />
     </div>
   );
 };
