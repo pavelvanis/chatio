212be8381bc880a4d3bbef624aca0f8ff11a86d7
vanis <pavlik.vanis@email.cz>
2024-01-07 19:59:21 +0100

    Remove member from server

---
+++ src/app/api/member-ships/delete.ts
@@ -0,0 +1,34 @@
+import { errorHandler } from "@/lib/services/apiErrorHandler";
+import MemberShipModel, { IMembership } from "@/models/member-ship";
+import { NextRequest, NextResponse } from "next/server";
+
+export const deleteOne = async (req: NextRequest) => {
+  try {
+    const { searchParams } = new URL(req.nextUrl);
+
+    const user = searchParams.get("userId");
+    const server = searchParams.get("serverId");
+
+    console.log("SERCH PARAMS", server, user);
+
+    if (!server) {
+      return new NextResponse("Server ID is missing!", { status: 400 });
+    }
+
+    if (!user) {
+      return new NextResponse("User ID is missing!", { status: 400 });
+    }
+
+    const deletedMembership = await MemberShipModel.findOneAndDelete({
+      user,
+      server,
+    });
+
+    // Return user ..
+    return NextResponse.json(deletedMembership);
+  } catch (error) {
+    console.error(error);
+    // Return error ..
+    return errorHandler(error);
+  }
+};
