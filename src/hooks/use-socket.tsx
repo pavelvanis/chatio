141aca15f51392bb95f838a623aebbaf1c071465
vanis <pavlik.vanis@email.cz>
2024-01-07 14:59:09 +0100

    Create hook for use socket

---
+++ src/hooks/use-socket.tsx
@@ -0,0 +1,85 @@
+import { useSocket } from "@/components/providers/socket-provider";
+import { IMessage } from "@/models/message";
+import { IUser } from "@/models/user";
+import { useQueryClient } from "@tanstack/react-query";
+import { pages } from "next/dist/build/templates/app-page";
+import { useEffect } from "react";
+
+type ChatSocketProps = {
+  addKey: string;
+  updateKey: string;
+  queryKey: string;
+};
+
+type MessageWithUser = IMessage & { userId: IUser };
+
+export const useChatSocket = ({
+  addKey,
+  updateKey,
+  queryKey,
+}: ChatSocketProps) => {
+  const { socket } = useSocket();
+  const queryClient = useQueryClient();
+
+  useEffect(() => {
+    if (!socket) {
+      return;
+    }
+
+    socket.on(updateKey, (message: MessageWithUser) => {
+      queryClient.setQueryData([queryKey], (oldData: any) => {
+        if (!oldData || !oldData.pages || oldData.pages.length === 0) {
+          return oldData;
+        }
+
+        const newData = oldData.pages.map((page: any) => {
+          return {
+            ...page,
+            items: page.items.map((item: MessageWithUser) => {
+              if (item.id === message.id) {
+                return message;
+              }
+              return item;
+            }),
+          };
+        });
+
+        return {
+          ...oldData,
+          pages: newData,
+        };
+      });
+    });
+
+    socket.on(addKey, (message: MessageWithUser) => {
+      queryClient.setQueryData([queryKey], (oldData: any) => {
+        if (!oldData || !oldData.pages || oldData.pages.length === 0) {
+          return {
+            pages: [
+              {
+                items: [message],
+              },
+            ],
+          };
+        }
+
+        const newData = [...oldData.pages];
+
+        newData[0] = {
+          ...newData[0],
+          items: [message, ...newData[0].items],
+        };
+
+        return {
+          ...oldData,
+          pages: newData,
+        };
+      });
+    });
+
+    return () => {
+      socket.off(addKey);
+      socket.off(updateKey);
+    };
+  }, [queryClient, addKey, queryKey, socket, updateKey]);
+};
