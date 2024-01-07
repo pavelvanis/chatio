e70e8d352cfdc0669b91429714dd3746e46b4099
vanis <pavlik.vanis@email.cz>
2024-01-07 16:44:02 +0100

    Fix returning messages

--- src/app/api/messages/route.ts
+++ src/app/api/messages/route.ts
@@ -2,7 +2,7 @@ import MessageModel, { IMessage } from "@/models/message";
 import { timeStamp } from "console";
 import { NextRequest, NextResponse } from "next/server";
 
-const MESSAGES_BATCH_SIZE = 10;
+const MESSAGES_BATCH_SIZE = 15;
 
 export async function GET(res: NextRequest) {
   try {
@@ -11,6 +11,9 @@ export async function GET(res: NextRequest) {
     const cursor = searchParams.get("cursor");
     const serverId = searchParams.get("serverId");
 
+    console.log(cursor);
+    console.log("SERCH PARAMS", searchParams);
+
     if (!serverId) {
       return new NextResponse("Server ID is missing!", { status: 400 });
     }
@@ -18,8 +21,10 @@ export async function GET(res: NextRequest) {
     let messages: any[] = [];
 
     if (cursor) {
-      messages = await MessageModel.find({ serverId: serverId })
-        .skip(1)
+      messages = await MessageModel.find({
+        serverId: serverId,
+        _id: { $lt: cursor },
+      })
         .limit(MESSAGES_BATCH_SIZE)
         .sort({ timestamp: "desc" })
         .populate({
@@ -34,9 +39,16 @@ export async function GET(res: NextRequest) {
 
     let nextCursor = null;
 
+    console.log("LENGTH", messages.length);
+    messages.forEach((element) => {
+      console.log(element.content);
+    });
+
     if (messages.length === MESSAGES_BATCH_SIZE) {
       nextCursor = messages[MESSAGES_BATCH_SIZE - 1].id;
     }
+
+    console.log("CURSOR", nextCursor);
 
     return NextResponse.json({ items: messages, nextCursor });
   } catch (error) {
